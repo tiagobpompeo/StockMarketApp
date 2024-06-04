faf2bde8bd7f18b07b0668004a5984e03b1a1896
Tiago Benaias Pompeo <tiapom@gmail.com>
2024-06-04 14:52:20 +0100

    Start Application

---
+++ StockMarketApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StockMarketApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ StockMarketApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StockMarketApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
