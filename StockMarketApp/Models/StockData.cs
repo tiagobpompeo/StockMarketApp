6bed71f53934b6fa722f3c139c38fd4eff31d9cf
Tiago Benaias Pompeo <tiapom@gmail.com>
2024-06-04 15:58:21 +0100

    GenericRepository

---
+++ StockMarketApp/Models/StockData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StockMarketApp.Models
+{
+    public class StockData
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
---
+++ StockMarketApp/ServiceAuthenticationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace StockMarketApp
+{
+    public class ServiceAuthenticationException : Exception
+    {
+        public string Content { get; set; }
+        public ServiceAuthenticationException(string content)
+        {
+            Content = content;
+        }
+    }
+}
+
